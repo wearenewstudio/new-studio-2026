16f007562a60c977cc1a2eb7b01785522ddafc81
kyryloren <kyrylo@outerlabs.studio>
2025-11-28 10:59:30 -0500

    Remove SectionReel and SectionTitle components from the application, and update index page to utilize SectionExpertise for improved content organization.

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -45,66 +45,7 @@
 
     <SectionReel :reel="home?.reel" />
 
-    <section class="pt-20 pb-40">
-      <SectionTitle
-        :title="'Expertise'"
-        :label="'Specializing in experiences'"
-      />
-
-      <div class="container flex flex-col gap-5">
-        <div class="custom-grid gap-y-5">
-          <p class="label col-start-1 col-end-13">Transformational Models</p>
-          <div class="line col-start-1 col-end-13" />
-          <div class="col-start-1 col-end-4">
-            <p class="medium indent-10">
-              We offer eight transformation pathways, each built around a
-              pivotal moment of change.
-            </p>
-          </div>
-          <div class="col-start-5 col-end-13 -mt-2 flex flex-col gap-1">
-            <p class="heading-1">Creation</p>
-            <p class="heading-1">Growth</p>
-            <p class="heading-1">Expansion</p>
-            <p class="heading-1">Repositioning</p>
-            <p class="heading-1">Modernization</p>
-            <p class="heading-1">Unification</p>
-            <p class="heading-1">Reinvention</p>
-            <p class="heading-1">Recovery</p>
-          </div>
-        </div>
-        <div class="custom-grid gap-y-5">
-          <p class="label col-start-1 col-end-13">Capabilities</p>
-          <div class="line col-start-1 col-end-13" />
-          <div class="col-start-1 col-end-4">
-            <p class="medium indent-10">
-              We specialize in crafting seamless and engaging brands, user
-              experiences and, functions.
-            </p>
-          </div>
-          <div class="col-start-5 col-end-13 -mt-2 flex flex-col gap-1">
-            <p class="heading-1">Strategy</p>
-            <p class="heading-1">Design</p>
-            <p class="heading-1">Development</p>
-            <p class="heading-1">Expression</p>
-          </div>
-        </div>
-        <div class="custom-grid gap-y-5">
-          <p class="label col-start-1 col-end-13">Awards</p>
-          <div class="line col-start-1 col-end-13" />
-          <div class="col-start-1 col-end-4">
-            <p class="medium indent-10">
-              Though we don’t believe awards are a measurement of success, we
-              got them.
-            </p>
-          </div>
-          <div class="col-start-5 col-end-13 -mt-2 flex flex-col gap-1">
-            <p class="medium">Communication Arts</p>
-            <p class="medium">Graphic Design USA (3x)</p>
-            <p class="medium">Awwwards (2x)</p>
-          </div>
-        </div>
-      </div>
-    </section>
+    <SectionExpertise />
   </div>
 </template>
 
